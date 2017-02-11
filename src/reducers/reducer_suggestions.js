29cbd437154432c1fa4b9243823a7f58c1d3789a
Jason Pham <s4354618@gmail.com>
2017-02-11 14:11:15 +1000

    Forgot to upload file (Build currently working), same as previous commit log

--- src/reducers/reducer_suggestions.js
+++ src/reducers/reducer_suggestions.js
@@ -1,4 +1,6 @@
 import { FETCH_SUGGESTION, TAB_SUGGESTION } from '../actions/index';
+import x2js from 'x2js';
+
 const INITAL_STATE = [];
 
 export default function(state = INITAL_STATE, action) {
@@ -6,8 +8,11 @@ export default function(state = INITAL_STATE, action) {
     case FETCH_SUGGESTION:
       return [ action.payload, ...state ];
     case TAB_SUGGESTION:
-      console.log(action);
       console.log("TAB SUGGESTION FIRED:");
+
+      const parser = new x2js();
+      console.log(parser.xml2js(action.payload.data).anime);
+
       return state;
     default:
       return state;
