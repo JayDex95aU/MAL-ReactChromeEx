adc6b10ed9d1405c1462c514a8462dd060618283
Jason Pham <s4354618@gmail.com>
2017-02-12 10:52:44 +1000

    Replaced bootstrap 4 with materializecss

--- src/components/app.js
+++ src/components/app.js
@@ -5,16 +5,28 @@ class App extends Component {
   render() {
     return(
       <div>
-        <nav className="navbar navbar-light bg-faded">
-          <a className="navbar-brand display-1">
-            <img src="./img/MAL_logo.png" width="30" height="30" className="rounded float-left" alt="" />
-            Title Here
-          </a>
-        </nav>
-        <div className="container">
-        {this.props.children}
-        </div>
+        <header>
+
+        </header>
+
+        <main className="container">{this.props.children}</main>
+
+        <footer class="page-footer">
+          
+        </footer>
       </div>
+
+      // <div>
+      //   <nav className="navbar navbar-light bg-faded">
+      //     <a className="navbar-brand display-1">
+      //       <img src="./img/MAL_logo.png" width="30" height="30" className="rounded float-left" alt="" />
+      //       Title Here
+      //     </a>
+      //   </nav>
+      //   <div className="container">
+      //   {this.props.children}
+      //   </div>
+      // </div>
     );
   }
 }
